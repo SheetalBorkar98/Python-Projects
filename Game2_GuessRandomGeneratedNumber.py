8f849bfec4981a21fb7d5a7369c9423fcefda80e
Sheetal Borkar <sheetalbborkar98@gmail.com>
2023-06-16 02:59:28 +0530

    slight changes

--- Game2_GuessRandomGeneratedNumber.py
+++ Game2_GuessRandomGeneratedNumber.py
@@ -8,19 +8,26 @@ count = 0
 
 while (user != randno):
     try:
-        user = int(input("Guess which number the computer might have choosed : "))
-        if user > randno:
-            print("Please choose a lower number..")
-            count += 1
-        elif user < randno:
-            print("Please Choose a bigger number..")
-            count += 1
+        user = int(input("\nGuess the number computer might have choosed (Press 0 to quit playing) : "))
+
+        if user == 0:
+            print("\nQuitting game...\n")
+            break
         else:
-            count += 1
+            if user > randno:
+                print("\nPlease choose a lower number..")
+                count += 1
+            elif user < randno and user >0:
+                print("\nPlease Choose a bigger number..")
+                count += 1
+            else:
+                count += 1
 
     except Exception as e:
-        print("Enter a valid integer...")
+        print("\nEnter a valid integer...")
 
-
-print(f"You guessed the correct number after {count} trails")
-print("Computer choose : ",randno)
+if user == 0:
+    pass
+else:
+    print("\nComputer choose : ",randno)
+    print(f"\nYou guessed the correct number after {count} trails")
